7b5caf3fd45e518dd563e3b7e6a62be122b5000c
flimmbark-source <flimmbark@gmail.com>
2026-01-06 16:46:43 +0200

    Consolidate module 1 grammar card

--- src/data/modules/index.ts
+++ src/data/modules/index.ts
@@ -10,7 +10,7 @@ const module1: LearningModule = {
   description: 'Learn basic greetings, how to introduce yourself, and simple present tense',
   order: 1,
   vocabTextIds: ['module-1-vocab-1', 'module-1-vocab-2', 'module-1-vocab-3'],
-  grammarTextId: 'module-1-grammar',
+  grammarTextId: 'module-1-grammar-1',
   sentenceIds: [
     'greetings-1',
     'greetings-2',
