2ceaba290e26d9c682843a0cffe418d38239b93f
flimmbark-source <flimmbark@gmail.com>
2026-05-08 10:12:45 +0300

    Import vocab journey word popup styles

--- src/components/bridgeBuilder/VocabJourneyPanel.jsx
+++ src/components/bridgeBuilder/VocabJourneyPanel.jsx
@@ -14,6 +14,7 @@ import {
 } from './vocabJourneyModel.js';
 import './VocabJourneyPanel.css';
 import './VocabJourneyMobileTray.css';
+import './VocabJourneyWordPopup.css';
 import { useLocalization } from '../../context/LocalizationContext.jsx';
 import { useLanguage } from '../../context/LanguageContext.jsx';
 import { loadBridgeBuilderWords } from '../../data/bridgeBuilder/words/index.js';
