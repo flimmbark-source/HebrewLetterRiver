99ea3540b9cf2e091e6b9896da66f60b9730ffda
flimmbark-source <flimmbark@gmail.com>
2026-05-21 13:28:39 +0300

    Add English pronouns pack scene strings

---
+++ src/data/packScenes/appLanguages/english/pronouns_01Strings.js
@@ -0,0 +1,40 @@
+// English app-language strings for pronouns_01.
+
+export const pronouns_01Strings = {
+  goalText: 'Choose the pronoun that matches the marked person or group.',
+  briefDescription:
+    'Your friend points to people in a small scene. Use the Hebrew pronoun that matches who is speaking, listening, or being shown.',
+
+  prompts: {
+    identify_i: '',
+    identify_you_m: '',
+    identify_you_f: '',
+    identify_he: '',
+    identify_she: '',
+    identify_we: '',
+    identify_they: '',
+    close_exchange: 'Close politely.',
+  },
+
+  supportMeanings: {},
+
+  lineSupportMeanings: {
+    friend_who_is_this: 'Who is this?',
+    friend_nice: 'Very nice.',
+    player_i: 'I.',
+    player_you_m: 'You, masculine.',
+    player_you_f: 'You, feminine.',
+    player_he: 'He.',
+    player_she: 'She.',
+    player_we: 'We.',
+    player_they: 'They.',
+    player_thank_you: 'Thank you.',
+    distractor_i_am_home: 'I am at home.',
+    distractor_my_father: 'My father.',
+  },
+
+  recapTemplates: {
+    intro: 'Nice work following who is who!',
+    learnedPrefix: 'You used:',
+  },
+};
