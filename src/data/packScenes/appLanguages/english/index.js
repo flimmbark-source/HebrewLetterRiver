2ed70f285ad8fd378c2198bf151dcac428f717a6
flimmbark-source <flimmbark@gmail.com>
2026-05-21 14:08:29 +0300

    Register English app strings for missing Foundations scenes

--- src/data/packScenes/appLanguages/english/index.js
+++ src/data/packScenes/appLanguages/english/index.js
@@ -1,5 +1,8 @@
 import { greetings_01Strings } from './greetings_01Strings.js';
+import { pronouns_01Strings } from './pronouns_01Strings.js';
+import { family_01Strings } from './family_01Strings.js';
 import { food_01Strings } from './food_01Strings.js';
+import { adjectives_01Strings } from './adjectives_01Strings.js';
 import { colors_01Strings } from './colors_01Strings.js';
 import { numbers_01Strings } from './numbers_01Strings.js';
 import { shopping_01Strings } from './shopping_01Strings.js';
@@ -10,7 +13,10 @@ export const englishAppStrings = {
   shared: englishShared,
   packs: {
     greetings_01: greetings_01Strings,
+    pronouns_01: pronouns_01Strings,
+    family_01: family_01Strings,
     food_01: food_01Strings,
+    adjectives_01: adjectives_01Strings,
     colors_01: colors_01Strings,
     numbers_01: numbers_01Strings,
     shopping_01: shopping_01Strings,
