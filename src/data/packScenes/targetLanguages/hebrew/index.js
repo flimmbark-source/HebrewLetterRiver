2143d7b7a43d3c0d007f7a0df4312c694ce4f8ef
flimmbark-source <flimmbark@gmail.com>
2026-05-21 13:30:02 +0300

    Register Hebrew target lines for missing Foundations scenes

--- src/data/packScenes/targetLanguages/hebrew/index.js
+++ src/data/packScenes/targetLanguages/hebrew/index.js
@@ -1,5 +1,8 @@
 import { greetings_01Lines } from './greetings_01Lines.js';
+import { pronouns_01Lines } from './pronouns_01Lines.js';
+import { family_01Lines } from './family_01Lines.js';
 import { food_01Lines } from './food_01Lines.js';
+import { adjectives_01Lines } from './adjectives_01Lines.js';
 import { colors_01Lines } from './colors_01Lines.js';
 import { numbers_01Lines } from './numbers_01Lines.js';
 import { shopping_01Lines } from './shopping_01Lines.js';
@@ -7,7 +10,10 @@ import { everyday_objects_01Lines } from './everyday_objects_01Lines.js';
 
 export const hebrewTargetLines = {
   greetings_01: greetings_01Lines,
+  pronouns_01: pronouns_01Lines,
+  family_01: family_01Lines,
   food_01: food_01Lines,
+  adjectives_01: adjectives_01Lines,
   colors_01: colors_01Lines,
   numbers_01: numbers_01Lines,
   shopping_01: shopping_01Lines,
